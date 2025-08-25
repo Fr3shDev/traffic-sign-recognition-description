f734cd6826521c62bc14fa96d2eebd8c918e9b26
Hans Weno <billyhans90@gmail.com>
2025-08-25 23:04:36 +0100

    Final task complete

--- src/model.py
+++ src/model.py
@@ -2,9 +2,63 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import models, layers
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 data_dir = 'data/GTSRB/Train'
 classes = os.listdir(data_dir)
 print("Number of classes:", len(classes))
 
+IMG_SIZE = 32
+X = []
+y = []
 
+for label in range(len(classes)):
+    folder_path = os.path.join(data_dir, str(label))
+    for img_name in os.listdir(folder_path):
+        img_path = os.path.join(folder_path, img_name)
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+        X.append(img)
+        y.append(label)
+
+X = np.array(X)
+y = np.array(y)
+
+# Normalize
+X = X / 255.0
+
+y = to_categorical(y, num_classes=len(classes))  
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(len(classes), activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=15, batch_size=64, validation_data=(X_val, y_val))
+
+#Evaluate the model
+loss, accuracy = model.evaluate(X_val, y_val)
+print("Validation Accuracy:", accuracy)
+
+y_pred = np.argmax(model.predict(X_val), axis=1)
+y_true = np.argmax(y_val, axis=1)
+
+cm = confusion_matrix(y_true, y_pred)
+sns.heatmap(cm, annot=False, cmap='Blues')
+
+# Bonus improvements
+datagen = ImageDataGenerator(rotation_range=10, width_shift_range=0.1, height_shift_range=0.1, zoom_range=0.1)
+datagen.fit(X_train)
